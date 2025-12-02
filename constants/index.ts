288fcaaa2bfc48a97672ca6800ba0e8e43e0c8ff
mhdswalih <ctmswalihmc@gmail.com>
2025-12-02 18:40:29 +0530

    secodecommit

--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,5 @@ import { Platform } from "react-native";
 
 
 export const API_URL = Platform.OS == 'android'? 'http://10.0.2.2:3000' : 'http://localhost:3000'
-
 export const CLOUDINARY_NAME="djvqmnkoe"
 export const CLOUDINARY_UPLOADS="images"
